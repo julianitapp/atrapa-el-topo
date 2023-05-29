1f4f95c1fd011f5194cc1f97b591e989ddf7d3be
julianitapp <123569438+julianitapp@users.noreply.github.com>
2023-05-29 11:57:47 -0500

    29/0523

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
+let posiciontopo= null;
+
+function cuadradoazar() {
+  cuadrado.forEach((nombredeclase) => {
+    nombredeclase.classlist.remove("topo");
+  });
+  letposicionalazar = cuadrado [Math.floor (Math.random()*9)]
+  posicionalazar.classlist,add("topo");
+
+  posicionalazar= posicionalazar.id;
