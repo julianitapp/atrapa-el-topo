6270b304d2b8d3c1fbcb86f896ec917080205c43
julianitapp <123569438+julianitapp@users.noreply.github.com>
2023-05-31 10:28:35 -0500

    3105

--- app.js
+++ app.js
@@ -10,7 +10,33 @@ function cuadradoazar() {
   cuadrado.forEach((nombredeclase) => {
     nombredeclase.classlist.remove("topo");
   });
-  letposicionalazar = cuadrado [Math.floor (Math.random()*9)]
+  let posicionalazar = cuadrado [Math.floor (Math.random()*9)]
   posicionalazar.classlist,add("topo");
 
-  posicionalazar= posicionalazar.id;
+  posiciontopo = posicionalazar.id;
+}
+cuadrado.forEach((identificador) => {
+  identificador,addEventListener("click",() => {
+    if (identificador.id === posiciontopo) {
+    resultado = resultado +1;
+    puntaje.textcontent = resultado;
+    posicionalazar = null;
+    }
+  });
+});
+function movertopo () {
+  tiempotopo = serInterval(cuadrado, 700);
+  }
+  movertopo();
+
+  function cuentaregresiva() {
+    tiempoactual==;
+    tiempofaltante.textContent = tiempoactual;
+    if (tiempoactual === 0) {
+      clearInterval(idtiempo);
+      clearInterval(tiempotopo);
+      alert("se acabo el tiempo, tu puntaje es" + resultado + "topos atrapados");
+    
+    }
+  } 
+  let idtiempo = setInterval(cuentaregresiva, 1000);
